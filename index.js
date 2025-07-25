a87df55707b604d8680626e677ba05ff80eb5c66
Aqsa <aqsa70729@gmail.com>
2025-07-25 21:36:05 +0500

    extra comments removed

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// let h1=document.getElementById('H1');
-// let h2=document.getElementById('H2');
-// let h3=document.getElementById('H3');
-// let g1=document.getElementById('G1');
-// let g2=document.getElementById('G2');
-// let g3=document.getElementById('G3');
+
 let homeScoreBox=document.getElementById('home-score');
 let guestScoreBox=document.getElementById('guest-score');
 let homeScore=0;
